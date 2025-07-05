1dd5586e377822f4cfb86bd33bf6934f09369366
Diego Rojas <guatilam@gmail.com>
2025-07-05 00:49:10 -0500

    feat: adds promise handlers for get functions
    abstracts into async function the filter logic for author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,50 +20,84 @@ public_users.post("/register", (req, res) => {
     return res.send('User "' + username + '" registered successfully').status(200);
 });
 
+let getBooks = new Promise((resolve, reject) => {
+    if (!books){
+        reject('No books found');
+    } else {
+        resolve(books);
+    }
+});
+
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    return res.status(200).json(books);
+    getBooks.then((value) => {
+        return res.status(200).json(value);
+    }, (error) => {
+        return res.status(404).json({ message: error });
+    });
 });
+
+async function getBookByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+        if (!books[isbn]){
+            reject('Book not found');
+        } else {
+            resolve(books[isbn]);
+        }
+    });
+}
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    isbn = parseInt(req.params.isbn);
-    if (!books[isbn]) {
-        return res.send('Book not found').status(404);
-    }
-    return res.status(200).json(books[isbn]);
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = parseInt(req.params.isbn);
+    await getBookByISBN(isbn).then((value) => {
+        return res.status(200).json(value);
+    }, (error) => {
+        return res.status(404).json({ message: error });
+    });
 });
 
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    let booksByAuthor = [];
-    for (const isbn in books) {
+async function getBooksByFilter(data) {
+    // Get filter by author or title
+    const filter = data.filter;
+    let booksByFilter = [];
+    for (const isbn in books){
         const book = books[isbn];
-        if (book.author === author){
-            booksByAuthor.push(book);
+        // Save book if the filter matches the parameter
+        if (book[filter] === data.parameter){
+            booksByFilter.push(book);
         }
     }
-    if (booksByAuthor.length < 1){
-        return res.send('No books found by that author').status(404);
-    }
-    return res.status(200).json(booksByAuthor);
+    // Handle no books found or send books found and return promise
+    return new Promise((resolve, reject) => {
+        if (booksByFilter.length < 1){
+            reject(`No books found by that ${filter}`);
+        } else {
+            resolve(booksByFilter);
+        }
+    });
+}
+
+// Get book details based on author
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    await getBooksByFilter({ filter: 'author', parameter: author})
+        .then((value) => {
+            return res.status(200).json(value);
+        }, (error) => {
+            return res.status(404).json({ message: error });
+        });
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
-    let booksByTitle = [];
-    for (const isbn in books) {
-        const book = books[isbn];
-        if (book.title === title){
-            booksByTitle.push(book);
-        }
-    }
-    if (booksByTitle.length < 1){
-        return res.send('No books found with that title.').status(404);
-    }
-    return res.status(200).json(booksByTitle);
+    await getBooksByFilter({ filter: 'title', parameter: title})
+        .then((value) => {
+            return res.status(200).json(value);
+        }, (error) => {
+            return res.status(404).json({ message: error });
+        });
 });
 
 //  Get book review
